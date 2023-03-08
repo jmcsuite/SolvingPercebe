c6b4b4a41b727b4fbeb214be4de8121991ce6cfe
jmcsuite <jmcsuite@hotmail.com>
2023-03-08 10:40:38 -0600

    Tarjan SCC

---
+++ Algoritmario/Juan/SCC.cpp
@@ -0,0 +1,76 @@
+const int maxn = // add maxn
+
+namespace Tarjan{
+    int scc=0; // Number of resulting scc;
+    bool inStack[maxn];
+    int lo[maxn];
+    int hi[maxn];
+    int vis[maxn];
+    stack<int> st;
+    int step=0;
+    int sz[maxn]; //Size of each component. Components are 0 indexed
+    vector<vll> comps; // Each component
+    int myComp[maxn]; // Maps each node to each component
+    unordered_set<ll> compVadj[maxn]; // New graph
+
+    void clear(int N){
+        scc=0;
+        step = 0;
+        for(int i=0; i<N; i++){
+            vis[i] =0;
+            inStack[i] =0;
+            sz[i] = 0;
+            myComp[i] =0;
+            compVadj[i].clear();
+        }
+        comps.clear();
+    }
+
+    int tarjan(int x, vector<vll>& vadj){
+        if(vis[x]) return maxn+2;
+        vis[x] = 1;
+        inStack[x] = 1;
+        st.push(x);
+        lo[x] = hi[x] = step++;
+        for(int y : vadj[x]){
+            if(inStack[y]){
+                lo[x] = min(lo[x], hi[y]);
+                continue;
+            }
+            lo[x] = min(lo[x], tarjan(y, vadj));
+        }
+
+        if(lo[x] == hi[x]){
+            int currSz = 0;
+            vll thisComp;
+            while(st.top() != x){
+                thisComp.push_back(st.top());
+                inStack[st.top()] = 0;
+                myComp[st.top()] = scc;
+                currSz++;
+                st.pop();
+            }
+            thisComp.push_back(x);
+            inStack[st.top()] = 0;
+            myComp[st.top()] = scc;
+            st.pop();
+            currSz++;
+            comps.push_back(thisComp);
+            sz[scc] = currSz;
+            scc++;
+        }
+        return lo[x];
+    }
+
+    void tarjanMain(int N, vector<vll>& vadj){ // Use this. 
+        for(int i=0; i<N; i++){  //Modify this if your nodes start from 1
+            if(vis[i] == 0) tarjan(i,vadj]);
+        }
+    }
+}
+        
+
+
+
+
+}
